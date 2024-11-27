a85f0e3cd0330213f6738f4e547ff8074c5a694f
Staso Stanislav <xctacx5@gmail.com>
2024-11-27 14:13:13 +0200

    HotFix

--- SecureMicroservices/Movies.Client/Controllers/MoviesController.cs
+++ SecureMicroservices/Movies.Client/Controllers/MoviesController.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using Movies.Client.ApiServices;
-using Movies.Client.Data;
 using Movies.Client.Models;
 
 namespace Movies.Client.Controllers
